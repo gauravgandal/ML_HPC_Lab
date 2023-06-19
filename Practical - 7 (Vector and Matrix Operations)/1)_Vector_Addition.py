06b35e9b2e4a85fbafabea90060799f52d6fbf6f
Gaurav14 <52061194+gauravgandal@users.noreply.github.com>
2023-06-19 10:33:00 +0530

    Create 1)_Vector_Addition.py

---
+++ Practical - 7 (Vector and Matrix Operations)/1)_Vector_Addition.py
@@ -0,0 +1,23 @@
+import random
+import numpy as np
+import time
+
+N = 10000000
+
+# Addition of two vectors
+a = [random.randint(0, N) for _ in range(N)]
+b = [random.randint(0, N) for _ in range(N)]
+c = np.zeros(N, dtype=int)
+
+start = time.time()
+for i in range(N):
+    c[i] = b[i] + a[i]
+duration = time.time() - start
+print("Serial vector addition:", duration)
+
+start = time.time()
+c = np.add(a, b)
+duration = time.time() - start
+print("Parallel vector addition:", duration)
+
+print("***********************************")
